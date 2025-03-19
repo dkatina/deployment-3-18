c2575f644dec35aa5cd9d70a351c1b0ee7ba6011
dkatina <108095842+dkatina@users.noreply.github.com>
2025-03-18 21:07:52 -0400

    adjusted swagger docs, and added redirect to docs

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,12 @@
+from flask import redirect
 from app import create_app
 from app.models import db
 
 app = create_app('ProductionConfig')
+
+@app.route("/", methods=['GET'])
+def home():
+    return redirect('/api/docs')
 
 with app.app_context():
     # db.drop_all()
